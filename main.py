ab6a86c7136d6c54aa3dd84f48acf348cda461c9
sergio barbosa <157991550+sergiobfj@users.noreply.github.com>
2025-12-01 22:32:36 -0300

    Fix indentation and formatting in main.py

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ with open("creds.json", "r") as file:
     creds = json.load(file)
 
 scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+
 creds_sheets = Credentials.from_service_account_info(creds, scopes=scopes)
-gc = gspread.authorize(creds_sheets) 
+
+gc = gspread.authorize(creds_sheets)
 
 #Atribuindo as informações do JSON a variáveis
 bot = telebot.TeleBot(creds['telegram']['bot_token'])
